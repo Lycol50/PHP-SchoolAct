53d05b73cabccbb447a54896329f08b5bbe52fe3
Lyco Tatierra <lycol50@gmail.com>
2021-03-17 12:11:26 +0800

    Update letters.php

--- Assignment2/letters.php
+++ Assignment2/letters.php
@@ -2,17 +2,17 @@
 $letter = "a"; // put any letter
 $vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
 $consonants = array("B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "Y", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "y");
-switch(true)
-{
+switch (true) {
     case in_array($letter, $vowels):
         echo "The letter is: " . $letter;
-        echo "<br> The letter is an vowel";
+        echo "The letter is an vowel";
         break;
     case in_array($letter, $consonants):
         echo "The letter is: " . $letter;
-        echo "<br> The letter is an consonant";
+        echo "The letter is an consonant";
         break;
     default:
         echo "The letter is: " . $letter;
-        echo "<br> invalid.";
+        echo "invalid.";
 }
+?>
