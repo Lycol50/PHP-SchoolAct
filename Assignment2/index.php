503b9989321f5b878216cb9533edd674afd17734
Lyco Tatierra <lycol50@gmail.com>
2021-03-17 12:14:16 +0800

    Update index.php

--- Assignment2/index.php
+++ Assignment2/index.php
@@ -15,15 +15,15 @@
         $age = $_POST['age'];
             if ($age <= 17) {
             echo "Your age is: " . $age;
-            echo "Sorry you are too young to vote.";
+            echo "<br> Sorry you are too young to vote.";
         }
         elseif ($age >= 60) {
             echo "Your age is: " . $age;
-            echo "You are qualified to vote.";
+            echo "<br> Sorry you are too old to vote.";
         }
         else {
             echo "Your age is: " . $age;
-            echo "You are too old to vote.";
+            echo "<br> You are qualified to vote.";
         }
     }
     ?>
