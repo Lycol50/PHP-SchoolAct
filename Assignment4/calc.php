8e5b90a55e18d867022c3441a171c48d82a5739f
starterdragon <papadie102@gmail.com>
2021-03-27 15:28:19 +0400

    Update calc.php

--- Assignment4/calc.php
+++ Assignment4/calc.php
@@ -1,7 +1,20 @@
+<html lang="en">
+<head>
+    <title>PHP Calculator</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+</head>
+<body style="font-family: Ubuntu; background-color: #151515; color: white; font-size: 20px;">
+<form method="post" action="">
+    Enter First Number: <input type="text" name="firstnum"><br>
+    Enter Second Number: <input type="text" name="secnum"><br>
+    <input type="submit" name="ADD" value="ADD"> <input type="submit" name="DIFF" value="DIFF"> <input type="submit" name="DIV" value="DIV">
+    <input type="submit" name="MULT" value="MUL">
+</form>
+    
 <?php
 // inputs and outputs
-
-if(!is_int($_POST['firstnum']) || !is_int($_POST['firstnum'])) return echo 'Invalid input';
+if(!is_int($_POST['firstnum']) || !is_int($_POST['secnum'])) return echo 'Invalid input';
 
 $array = array(
     'first' => $_POST['firstnum'],
@@ -53,19 +66,7 @@ switch (true) {
 }
 
 ?>
-<html lang="en">
-<head>
-    <title>PHP Calculator</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
-</head>
-<body style="font-family: Ubuntu; background-color: #151515; color: white; font-size: 20px;">
-<form method="post" action="">
-    Enter First Number: <input type="text" name="firstnum"><br>
-    Enter Second Number: <input type="text" name="secnum"><br>
-    <input type="submit" name="ADD" value="ADD"> <input type="submit" name="DIFF" value="DIFF"> <input type="submit" name="DIV" value="DIV">
-    <input type="submit" name="MULT" value="MUL">
-</form>
+
 <footer><h6>Code by princepines<br>Available on <a
                 href="https://github.com/Lycol50/PHP-SchoolAct/blob/main/Assignment4/calc.php">Github</a></h6></footer>
 </body>
